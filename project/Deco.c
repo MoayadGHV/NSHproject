1ad68bfac536d7936588d2ebe2c9897ebe0b17f1
Bader935 <Bader.almutairi935@gmail.com>
2024-11-06 09:57:11 +0300

    Update the first commit

--- project/Deco.c
+++ project/Deco.c
@@ -2,9 +2,14 @@
 
 	//int Dec2to4();
 	void decoder2to4(int input0, int input1, int *output0, int *output1, int *output2, int *output3) {
-    	*output0 = (input0 == 0 && input1 == 0);
-    	*output1 = (input0 == 0 && input1 == 1);
-    	*output2 = (input0 == 1 && input1 == 0);
-    	*output3 = (input0 == 1 && input1 == 1);
+    	// Restore it in RA
+		*output0 = (input0 == 0 && input1 == 0);
+    	// restore in RB
+		*output1 = (input0 == 0 && input1 == 1);
+    	// restore in RO
+		*output2 = (input0 == 1 && input1 == 0);
+    	
+	// NOTHING to DO 	
+	*output3 = (input0 == 1 && input1 == 1);
 	}
 
